b601d00889a353b8706c176f2f80f7c405591227
Jay Ann Lomod <jaqlomod@gmail.com>
2021-10-29 04:05:43 +0800

    changed key to 'key'

--- waissapp/views.py
+++ waissapp/views.py
@@ -101,7 +101,8 @@ def new_calib(request):
 		calib.date_tested = request.POST.get('date_tested')
 		calib.tested_by = request.POST.get('tested_by')
 		for key in request.POST:
-			value = request.POST.getlist('key')
+			value = request.POST.getlist(key)
+			print(value)
 			if value == "":
 				pass
 			else:
