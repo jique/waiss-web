3c1bf458140b845d9a0e2c55991a0e4dd2ebc4cc
Jay Ann Lomod <jaqlomod@gmail.com>
2021-10-28 15:45:06 +0800

    changed layout new_farm

--- waissapp/forms.py
+++ waissapp/forms.py
@@ -112,12 +112,6 @@ class MCForm(forms.ModelForm):
         exclude = ('author', 'personal',)  # this says to include all fields from model to the form 
 
 class FarmForm(ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(FarmForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = 'form-horizontal'
-        self.helper.label_class = 'col-5'
-        self.helper.field_class = 'col-7' 
     class Meta:
         model = Farm
         exclude = ('author', 'personal',)  # this says to include all fields from model to the form 
