f897eb12f43e247895d96dd8cefd8261749a763c
Jay Ann Lomod <jaqlomod@gmail.com>
2021-11-02 17:33:14 +0800

    extras in formset

--- waissapp/views.py
+++ waissapp/views.py
@@ -566,7 +566,7 @@ def new_sensor(request):
 				sensors, created = Sensor.objects.get_or_create(**data)
 				sensors.depth = form.cleaned_data.get('depth')
 				sensors.save()
-			return redirect('/new_system/')
+				return redirect('/new_system/')
 	
 	context = {
 		"formset": formset,
