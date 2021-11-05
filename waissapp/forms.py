5c897c3f16989db860b1280da44b5c96cd92ac31
Jay Ann Lomod <jaqlomod@gmail.com>
2021-11-06 00:46:21 +0800

    phonenumberwidget

--- waissapp/forms.py
+++ waissapp/forms.py
@@ -52,8 +52,7 @@ class CropForm(forms.ModelForm):
 class FieldUnitForm(forms.ModelForm):
     timestart = forms.TimeField(widget=forms.TextInput(attrs={'type': 'time'}), label="Start Time", required=True)
     timestop = forms.TimeField(widget=forms.TextInput(attrs={'type': 'time'}), label="Stop Time", required=True)
-    number = PhoneNumberField(
-        widget=PhoneNumberPrefixWidget, required=True)
+    number = PhoneNumberField(widget=PhoneNumberPrefixWidget)
     def __init__(self, *args, **kwargs):
         super(FieldUnitForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
