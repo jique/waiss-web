ddd4f261170b1ba345b9c608db7e5e20070b6db1
Jay Ann Lomod <jaqlomod@gmail.com>
2021-11-09 16:37:23 +0800

    home page map

--- waiss/settings.py
+++ waiss/settings.py
@@ -89,6 +89,7 @@ WSGI_APPLICATION = 'waiss.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
