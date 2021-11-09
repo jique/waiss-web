a53591148c2cbd71cedbd89aad6e9b8f5b7b8983
Jay Ann Lomod <jaqlomod@gmail.com>
2021-11-09 19:31:31 +0800

    modal farm remove select_coordinate fields

--- waiss/settings.py
+++ waiss/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.gis',
     'django.contrib.staticfiles',
     'crispy_forms',
     'leaflet',
@@ -87,7 +88,7 @@ WSGI_APPLICATION = 'waiss.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
