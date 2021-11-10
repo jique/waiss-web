d98c047be0ec0bf62c939e23a1f33ce9613fe2dc
Jay Ann Lomod <jaqlomod@gmail.com>
2021-11-11 00:08:26 +0800

    csv reformat

--- waissapp/computations.py
+++ waissapp/computations.py
@@ -523,7 +523,7 @@ def index(request):
 			net_application_depth = net_application_depth * Pw
 			gross_application_depth = net_application_depth*transpiration_ratio/eu
 			print(Pd, Etcrop, bln_ii, transpiration_ratio )
-			if bln_ii == True:
+			if bln_ii == yes:
 				irrigation_interval = float(drip.irrigation_interval)
 			else:
 				Td = Etcrop*(Pd + 0.15*(1 - Pd))
